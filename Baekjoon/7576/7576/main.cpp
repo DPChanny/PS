2bb3ffeaebe23395e02c9815d8f685e85fe4c694
Park Kyoung Chan <kyoungchan0516@naver.com>
2021-12-25 21:06:25 +0900

    2021/12/25-21:06

--- Baekjoon/7576/7576/main.cpp
+++ Baekjoon/7576/7576/main.cpp
@@ -7,13 +7,6 @@ int ts[1000][1000],
 	x,
 	y;
 
-bool check_point(int _y, int _x) {
-	return 
-		_y + 1 > 0 && _y < y && 
-		_x + 1 > 0 && _x < x && 
-		!ts[_y][_x];
-}
-
 int main(void) {
 	int t(0), s;
 	cin >> x >> y;
@@ -21,8 +14,8 @@ int main(void) {
 	for (int _y(0); _y < y; _y++)
 		for (int _x(0); _x < x; _x++) {
 			cin >> ts[_y][_x];
-			if (ts[_y][_x])
-				q.push(make_pair(_y, _x));
+			if (ts[_y][_x] == 1)
+				q.push({_y, _x});
 		}
 	pair<int, int> c;
 	while (!q.empty()) {
@@ -30,22 +23,30 @@ int main(void) {
 		for (int _s(0); _s < s; _s++) {
 			c = q.front(); q.pop();
 			ts[c.first][c.second] = 1;
-			if (check_point(c.first + 1, c.second + 1))
-				q.push(make_pair(c.first + 1, c.second + 1));
-			if (check_point(c.first + 1, c.second - 1))
-				q.push(make_pair(c.first + 1, c.second - 1));
-			if (check_point(c.first - 1, c.second + 1))
-				q.push(make_pair(c.first - 1, c.second + 1));
-			if (check_point(c.first - 1, c.second - 1))
-				q.push(make_pair(c.first - 1, c.second - 1));
+			if (c.first + 1 < y && !ts[c.first + 1][c.second]) {
+				ts[c.first + 1][c.second] = 1;
+				q.push({ c.first + 1, c.second });
+			}
+			if (c.first > 0 && !ts[c.first - 1][c.second]) {
+				ts[c.first - 1][c.second] = 1;
+				q.push({ c.first - 1, c.second });
+			}
+			if (c.second + 1 < x && !ts[c.first][c.second + 1]) {
+				ts[c.first][c.second + 1] = 1;
+				q.push({ c.first, c.second + 1 });
+			}
+			if (c.second > 0 && !ts[c.first][c.second - 1]) {
+				ts[c.first][c.second - 1] = 1;
+				q.push({ c.first, c.second - 1 });
+			}
 		}
 		t++;
 	}
 	for (int _y(0); _y < y; _y++)
 		for (int _x(0); _x < x; _x++)
 			if (!ts[_y][_x]) {
-				cout << '-1';
+				cout << -1;
 				return 0;
 			}
-	cout << t;
+	cout << t - 1;
 }
