66e5f304f05bc956493b27fdc5712c7ded6dea6e
Park Kyoung Chan <kyoungchan0516@naver.com>
2022-01-12 23:19:03 +0900

    2022/01/12-23:18

--- Baekjoon/2585/2585/main.cpp
+++ Baekjoon/2585/2585/main.cpp
@@ -1,32 +1,33 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<cmath>
+#include<cstring>
 
 using namespace std;
 
 int n, k, bx, by, s;
-long long int ds[1000][1000];
-bool v[1000]{ false, };
+long long int ds[1002][1002];
+bool v[1002]{ false, };
 vector<pair<int, int>> ns;
 
-//최대 거리 _md로 출발 지점에서 도착 지점까지 갈 수 있는지 체크
 bool check(long long int _md) {
 	memset(v, false, sizeof(v));
 	int _k(0);
 	queue<int> q;
 	q.push(0);
-	while (!q.empty() && _k++ < k) {
+	while (!q.empty() && _k++ < k + 1) {
 		s = q.size();
 		for (int _s(0); _s < s; _s++) {
 			for (int _n(1); _n < ns.size(); _n++) {
 				if (v[_n]) continue;
 				if (ds[q.front()][_n] > _md) continue;
+				if (_n == ns.size() - 1) return true;
 				q.push(_n);
 				v[_n] = true;
-				q.pop();
 			}
+			q.pop();
 		}
-		if (v[ns.size() - 1]) return true;
 	}
 	return false;
 }
@@ -41,9 +42,10 @@ int main(void) {
 	ns.push_back(make_pair(10000, 10000));
 	long long int d;
 	for (int _f(0); _f < ns.size(); _f++) {
-		for (int _t(0); _t < ns.size(); _t++) {
+		for (int _t(_f); _t < ns.size(); _t++) {
 			d = pow(ns[_f].first - ns[_t].first, 2) + pow(ns[_f].second - ns[_t].second, 2);
 			ds[_f][_t] = d;
+			ds[_t][_f] = d;
 		}
 	}
 	int fl(1), fm(14143), m;
