743cc705d85d6eb86f13547aef9ac1c4e7805829
Park Kyoung Chan <kyoungchan0516@naver.com>
2022-01-25 23:03:57 +0900

    2022/01/25-23:03

---
+++ Baekjoon/1005/1005/main.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+
+using namespace std;
+
+uint16_t n, w, bf, bt;
+vector<uint16_t> ks[1000];
+uint32_t t, k, ns[1000];
+uint64_t vs[1000];
+
+uint64_t dfs(uint16_t _n) {
+	if(!vs[_n]) {
+		if(ks[_n].empty()) vs[_n] = ns[_n];
+		else {
+			uint64_t m(0);
+			for(uint32_t _k(0); _k < ks[_n].size(); _k++)
+				m = max(m, dfs(ks[_n][_k]));
+			vs[_n] = m + ns[_n];
+		}
+	}
+	return vs[_n];
+}
+
+int32_t main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> t;
+	for(uint32_t _t(0); _t < t; _t++) {
+		cin >> n >> k;
+		for(uint16_t _n(0); _n < n; _n++)
+			cin >> ns[_n];
+		for(uint32_t _k(0); _k < k; _k++) {
+			cin >> bf >> bt;
+			ks[bt - 1].push_back(bf - 1);
+		}
+		cin >> w;
+		cout << dfs(w - 1) << '\n';
+		memset(vs, 0, sizeof(vs));
+		for(uint16_t _n(0); _n < n; _n++)
+			ks[_n].clear();
+	}
+}
