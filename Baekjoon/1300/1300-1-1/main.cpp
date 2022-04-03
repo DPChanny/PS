b4922ecd06554a77201c7dda3fb1bd5db598dfd9
jaeseok01 <jaeseok0701@gmail.com>
2022-04-03 10:44:22 +0900

    2022/04/03-10:44

--- Baekjoon/1300/1300-1-1/main.cpp
+++ Baekjoon/1300/1300-1-1/main.cpp
@@ -7,7 +7,7 @@ long long int n, k;
 long long int gc(const long long int& _i) {
 	long long int s(0);
 	for (long long int _n(1); _n < n + 1 && _i / _n; _n++)
-		s += min(_i / _n - (_i % _n ? 0 : 1), n);                                                                                                                                                                          -(_i % n ? 0 : 1);
+		s += min(_i / _n - (_i % _n ? 0 : 1), n);
 	return s;
 }
 
