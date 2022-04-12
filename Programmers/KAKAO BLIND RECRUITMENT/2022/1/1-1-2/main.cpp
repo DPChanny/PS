3ba5ce466a8e9408a48dc55a60734d75991322bd
Kyoung Chan Park <kyoungchan0516@naver.com>
2022-04-12 16:09:21 +0900

    2022/04/12-16:09

---
+++ Programmers/KAKAO BLIND RECRUITMENT/2022/1/1-1-2/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <map>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<string> _names, vector<string> _reports, int _k) {
+    map<string, vector<string>> reports;
+    map<string, int> report_counts;
+    map<string, map<string, bool>> reporteds;
+    for (size_t _report(0); _report < _reports.size(); _report++) {
+        string
+            from = _reports[_report].substr(0, _reports[_report].find(' ')),
+            to = _reports[_report].substr(_reports[_report].find(' '));
+        if (!reporteds[from][to]) {
+            reporteds[from][to] = true;
+            reports[from].push_back(to);
+            report_counts[to]++;
+        }
+    }
+    vector<int> result(_names.size());
+    for (size_t _name(0); _name < _names.size(); _name++)
+        for (size_t _report(0); _report < reports[_names[_name]].size(); _report++)
+            if (report_counts[reports[_names[_name]][_report]] + 1 > _k) result[_name]++;
+    return result;
+}
