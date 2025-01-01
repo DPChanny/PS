6d12ab0ac574cffde20a7c0e65e5e5c16620fa1b
DPChanny <kyoungchan0516@gmail.com>
2025-01-02 05:10:19 +0900

    4179 trial

--- Baekjoon/4179/4179/main.cpp
+++ Baekjoon/4179/4179/main.cpp
@@ -3,70 +3,90 @@
 
 using namespace std;
 
-#define WALL '#'
-#define VOID '.'
-#define FIRE 'F'
-#define BODY 'J'
+constexpr char WALL = '#';
+constexpr char VOID = '.';
+constexpr char FIRE = 'F';
+constexpr char BODY = 'J';
 
-char m[1000][1000];
-int y, x;
+char map[1000][1000];
+int y_size, x_size;
 
-struct M {
+struct Vector {
 	int y, x;
-	bool f;
 };
 
-bool check(M _m) {
+static bool check_position(Vector _position) {
+	return
+		_position.y + 1 > 0 && _position.y < y_size &&
+		_position.x + 1 > 0 && _position.x < x_size &&
+		map[_position.y][_position.x] != WALL;
+}
+
+static bool check_fire_position(Vector _position) {
 	return 
-		_m.y + 1 > 0 && _m.y < y &&
-		_m.x + 1 > 0 && _m.x < x &&
-		m[_m.y][_m.x] != WALL &&
-		m[_m.y][_m.x] != FIRE &&
-		(_m.f ? true : (m[_m.y][_m.x] != BODY));
+		check_position(_position) && 
+		map[_position.y][_position.x] != FIRE;
+}
+
+static bool check_body_position(Vector _position) {
+	return
+		check_fire_position(_position) && 
+		map[_position.y][_position.x] != BODY;
 }
 
 int main(void) {
-	int s, t(0);
-	cin >> y >> x;
-	queue<M> q;
-	for (int _y(0); _y < y; _y++) {
+	bool flag(false);
+	int queue_size, trial(0);
+	Vector position_buffer;
+	char map_buffer;
+	cin >> y_size >> x_size;
+	queue<Vector> next_body_positions, next_fire_positions;
+
+	for (int y(0); y < y_size; y++) {
 		getchar();
-		for (int _x(0); _x < x; _x++) {
-			m[_y][_x] = getchar();
-			if (m[_y][_x] == FIRE)
-				q.push({ _y, _x, true });
-			if (m[_y][_x] == BODY)
-				q.push({ _y, _x, false });
+		for (int x(0); x < x_size; x++) {
+			map_buffer = getchar();
+			map[y][x] = VOID;
+			if (map_buffer == FIRE) next_fire_positions.push({ y, x });
+			if (map_buffer == BODY) next_body_positions.push({ y, x });
+			if (map_buffer == WALL) map[y][x] = WALL;
 		}
 	}
-	M c;
-	while (!q.empty()) {
-		t++;
-		s = q.size();
-		for (int _s(0); _s < s; _s++) {
-			c = q.front(); q.pop();
-			if (!c.f && m[c.y][c.x] == FIRE) continue;
-			if (!c.f && (!c.y || c.y + 1 == y || !c.x || c.x + 1 == x)) {
-				cout << t;
-				return 0;
+
+	while (!next_body_positions.empty()) {
+		trial++;
+
+		queue_size = next_fire_positions.size();
+		for (int i(0); i < queue_size; i++) {
+			position_buffer = next_fire_positions.front(); next_fire_positions.pop();
+			if (map[position_buffer.y][position_buffer.x] == FIRE) continue;
+			map[position_buffer.y][position_buffer.x] = FIRE;
+
+			if (check_fire_position({ position_buffer.y + 1, position_buffer.x })) next_fire_positions.push({ position_buffer.y + 1, position_buffer.x });
+			if (check_fire_position({ position_buffer.y - 1, position_buffer.x })) next_fire_positions.push({ position_buffer.y - 1, position_buffer.x });
+			if (check_fire_position({ position_buffer.y, position_buffer.x + 1 })) next_fire_positions.push({ position_buffer.y, position_buffer.x + 1 });
+			if (check_fire_position({ position_buffer.y, position_buffer.x - 1 })) next_fire_positions.push({ position_buffer.y, position_buffer.x - 1 });
+		}
+
+		queue_size = next_body_positions.size();
+		for (int i(0); i < queue_size; i++) {
+			position_buffer = next_body_positions.front(); next_body_positions.pop();
+			if (map[position_buffer.y][position_buffer.x] == FIRE) continue;
+			if (map[position_buffer.y][position_buffer.x] == BODY) continue;
+			map[position_buffer.y][position_buffer.x] = BODY;
+
+			if (position_buffer.y + 1 == y_size || position_buffer.x + 1 == x_size || position_buffer.y == 0 || position_buffer.x == 0){
+				flag = true;
+				break;
 			}
-			if (check({ c.y + 1, c.x, c.f })) {
-				m[c.y + 1][c.x] = c.f ? FIRE : BODY;
-				q.push({ c.y + 1, c.x, c.f });
-			}
-			if (check({ c.y - 1, c.x, c.f })) {
-				m[c.y - 1][c.x] = c.f ? FIRE : BODY;
-				q.push({ c.y - 1, c.x, c.f });
-			}
-			if (check({ c.y, c.x + 1, c.f })) {
-				m[c.y][c.x + 1] = c.f ? FIRE : BODY;
-				q.push({ c.y, c.x + 1, c.f });
-			}
-			if (check({ c.y, c.x - 1, c.f })) {
-				m[c.y][c.x - 1] = c.f ? FIRE : BODY;
-				q.push({ c.y, c.x - 1, c.f });
-			}
+
+			if (check_body_position({ position_buffer.y + 1, position_buffer.x })) next_body_positions.push({ position_buffer.y + 1, position_buffer.x });
+			if (check_body_position({ position_buffer.y - 1, position_buffer.x })) next_body_positions.push({ position_buffer.y - 1, position_buffer.x });
+			if (check_body_position({ position_buffer.y, position_buffer.x + 1 })) next_body_positions.push({ position_buffer.y, position_buffer.x + 1 });
+			if (check_body_position({ position_buffer.y, position_buffer.x - 1 })) next_body_positions.push({ position_buffer.y, position_buffer.x - 1 });
 		}
 	}
-	cout << "IMPOSSIBLE";
+
+	if (flag) cout << trial;
+	else cout << "IMPOSSIBLE";
 }
